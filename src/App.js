c26ab097284f7748e0131d4d8e9ea769a9ec4426
Andrii Mutylo <amutylo@gmail.com>
2018-04-05 21:36:13 +0300

    added react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import CanvasCity from './canvascity';
 import './App.css';
+import Navbar from "./components/navbar/Navbar";
+import Home from './components/Home';
+import Product from './components/Product';
+import Brands from './component/Brands';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 
 class App extends Component {
   render() {
     return (
-          <CanvasCity></CanvasCity>
+      <div>
+      {/*<Navbar/>*/}
+      <Router>
+        <Route exact path="/" component={Home}/>
+        <Route path="/brands" component={Brands}/>
+        {/*<Route path="/applications" component={Applications}/>*/}
+        <Route path="/product/:id" component={Product}/>
+      </Router>
+      </div>
+          {/*<CanvasCity></CanvasCity>*/}
     );
   }
 }
